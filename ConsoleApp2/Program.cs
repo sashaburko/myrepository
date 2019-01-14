b38c2bef1f5f8f7e3aa4e9835a1395d31f4ec0d2
lPablol <pavlo.kundis@gmail.com>
2019-01-14 12:16:41 +0200

    Created money object in program main

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using money;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,7 @@ namespace ConsoleApp2
         }
         static void Main(string[] args)
         {
+            Money m = new Money(5,4);
         }
     }
 }
