f8a5c640e2ee8552afad5b487547ad7324d5f99c
lPablol <pavlo.kundis@gmail.com>
2019-01-14 12:19:26 +0200

    CreditCard

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mydll;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,7 @@ namespace ConsoleApp2
         }
         static void Main(string[] args)
         {
-            Purse p = new Purse();
+            CreditCard c = new CreditCard();
         }
     }
 }
