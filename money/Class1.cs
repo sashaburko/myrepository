406d71340de248be00a61b2a9d117c200c8b1905
lPablol <pavlo.kundis@gmail.com>
2019-01-14 11:59:21 +0200

    initialize Money

---
+++ money/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace money
+{
+    public class Money
+    {
+        int banktones;
+        int count;
+        public Money(int b, int c)
+        {
+            banktones = b;
+            count = c;
+        }
+        void ShowInfoMoney(Money[] purse)
+        {
+            foreach(Money i in purse)
+            {
+                Console.WriteLine("Banknote: {0} dol. {1} count", i.banktones, i.count);
+            }
+        }
+    }
+
+}
