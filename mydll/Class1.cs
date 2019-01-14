984b3ecda88c9817fe16bbf65524fb15ab07c949
lPablol <pavlo.kundis@gmail.com>
2019-01-14 12:11:53 +0200

    initialize CreditCard

---
+++ mydll/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mydll
+{
+    public class CreditCard
+    {
+        int count;
+        void Show()
+        {
+            Console.WriteLine("Count credit card:{0}", count);
+        }
+    }
+}
